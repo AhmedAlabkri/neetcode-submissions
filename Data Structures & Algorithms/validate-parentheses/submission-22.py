0ee520f162a34c752ba9140903fb7be17bc2eac0
AhmedAlabkri <123089485+AhmedAlabkri@users.noreply.github.com>
2026-09-01 00:51:38 +1000

    Add: validate-parentheses - submission-22

---
+++ Data Structures & Algorithms/validate-parentheses/submission-22.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        my_dict = {"}":"{", "]":"[", ")":"("}
+
+
+        for char in s:
+            if char not in my_dict:
+                stack.append(char)
+            else:
+                if stack and stack[-1] == my_dict[char]:
+                    stack.pop()
+                else:
+                    return False
+        if not stack:
+            return True
+        return False
